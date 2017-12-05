e2c4f7d86b22943d1a2878de53592482918d2f3c
Richard Jones <rjones30@gmail.com>
2017-12-05 16:34:51 -0500

    * TAGMtrees.bash, TAGMtrees.sub [rtj]    - update the scripts for running hd_root with the TAGM_trees plugin      on the UConn cluster, new evio files locations, etc.

--- TAGMtrees.bash
+++ TAGMtrees.bash
@@ -6,12 +6,13 @@ source setup.sh
 echo $LD_LIBRARY_PATH
 ldd `which hd_root`
 
-rawdata=/pnfs4/phys.uconn.edu/data/Gluex/commissioning/rawdata
+pnfspath=/pnfs4/phys.uconn.edu/data
+rawdata=/Gluex/calibration/TAGM/rawdata
 
 if [[ $# > 0 ]]; then
     line=`expr $1 + 1`
     run=`head -n $line runs.list | tail -n 1`
-    export infile=`ls $rawdata/Run0$run/*.evio`
+    export infile=`ls $pnfspath/$rawdata/Run0$run/*.evio`
 else
     echo "usage: run_hdroot.sh <sequence number>"
     exit 1
@@ -21,16 +22,15 @@ if [[ ! -r $infile ]]; then
     exit 1
 fi
 
-echo $HALLD_HOME/$BMS_OSNAME/bin/hd_root -PJANA:BATCH_MODE=1 -PPRINT_PLUGIN_PATHS=1 -PPLUGINS=TAGM_trees -PTHREAD_TIMEOUT_FIRST_EVENT=3000 -PTHREAD_TIMEOUT=3000 $infile
+echo $HALLD_HOME/$BMS_OSNAME/bin/hd_root -PJANA:BATCH_MODE=1 -PPRINT_PLUGIN_PATHS=1 -PPLUGINS=TAGM_trees -PTHREAD_TIMEOUT_FIRST_EVENT=3000 -PTHREAD_TIMEOUT=3000 -PEVIO:RUN_NUMBER=$run $infile
 $HALLD_HOME/$BMS_OSNAME/bin/hd_root \
   -PPRINT_PLUGIN_PATHS=1 \
   -PJANA:BATCH_MODE=1 \
   -PPLUGINS=TAGM_trees \
-  -PTT:SYSTEMS_TO_PARSE=TAGM \
+  -PEVIO:RUN_NUMBER=$run \
   -PTHREAD_TIMEOUT_FIRST_EVENT=300 \
   -PTHREAD_TIMEOUT=300 \
   $infile
-#  -PEVIO:VERBOSE=99 \
 retcode=$?
 
 if [[ $retcode = 0 ]]; then
