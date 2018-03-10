10c8a48e69a39c3b00adcdf8c3af932b02187ef2
Richard Jones <rjones30@gmail.com>
2018-03-10 08:56:32 -0500

    * correct the options to setVbias to match the specifics of the dark box   setup in P-403 at UConn.

--- do_sequence.sh
+++ do_sequence.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #
 # do_sequence.sh - script to cycle through the the rows in a fiber bundle
-#                  and set the bias voltages on by row or by individual sipm,
-#                  ending with all bias voltages reset to zero.
+#	              and set the bias voltages on by row or by individual sipm,
+#	              ending with all bias voltages reset to zero.
 #
 # author: richard.t.jones at uconn.edu
 # version: november 21, 2015
@@ -17,63 +17,63 @@ V0=50
 netdev=gluon2.phys.uconn.edu:
 
 while [[ $# -gt 0 ]]; do
-    if `echo $1 | grep -q -- -d`; then
+	if `echo $1 | grep -q -- -d`; then
 	delay=`echo $1 | awk '/-d/{print substr($1,3)}'`
-        shift
-        if [[ $delay = "" ]]; then
-	    delay=$1
-	    shift
-        fi
-    elif `echo $1 | grep -q -- -g`; then
+		shift
+		if [[ $delay = "" ]]; then
+		delay=$1
+		shift
+		fi
+	elif `echo $1 | grep -q -- -g`; then
 	gain_pC=`echo $1 | awk '/-g/{print substr($1,3)}'`
-        shift
-        if [[ $gain_pC = "" ]]; then
-	    gain_pC=$1
-	    shift
-        fi
-    elif `echo $1 | grep -q -- -r`; then
+		shift
+		if [[ $gain_pC = "" ]]; then
+		gain_pC=$1
+		shift
+		fi
+	elif `echo $1 | grep -q -- -r`; then
 	byrow=1
-        shift
-    elif `echo $1 | grep -q -- -H`; then
+		shift
+	elif `echo $1 | grep -q -- -H`; then
 	opts=-H
-        shift
-    elif `echo $1 | grep -q -- -L`; then
+		shift
+	elif `echo $1 | grep -q -- -L`; then
 	opts=-L
-        shift
-    else
+		shift
+	else
 	echo "Usage: do_sequence [-r] [-H | -L] [-d <delay_sec>] [-g <gain_pC>]"
 	exit 0
-    fi
+	fi
 done
 
 tracefile=/tmp/ether_trace.log
 cat /dev/null >$tracefile
 
 cd ~halld/online/TAGMutilities
-bin/setVbias $opts -c 1-102 -r 1-5 -V $V0 $netdev >/dev/null
+bin/setVbias $opts -c 1-6 -r 1-5 -V $V0 $netdev >/dev/null
 
 if [[ $byrow != "" ]]; then
-    for row in 1 2 3 4 5; do
-	echo "lighting up row $row"
-	bin/setVbias $opts -c 1-102 -r $row -g $gain_pC $netdev >> $tracefile
-	ssh halld@halldtrg5 mqwrite /Vbias 0x0$row
-	sleep $delay
-	ssh halld@halldtrg5 mqwrite /Vbias 0xff
-	bin/setVbias $opts -c 1-102 -r 1-5 -V $V0 $netdev >/dev/null
-    done
-else
-    for col in 1 2 3; do
 	for row in 1 2 3 4 5; do
-	    chan=`echo $row $col | awk '{printf("%2.2x", 1+($1-1)+($2-1)*5)}'`
-            cols=`echo $col | awk '{printf("%d,%d", $1, $1+3)}'`
-	    echo "selecting channel $chan"
-	    bin/setVbias $opts -c $cols -r $row -g $gain_pC $netdev >>$tracefile
-	    ssh halld@halldtrg5 mqwrite /Vbias 0x01$chan
-	    sleep $delay
-	    ssh halld@halldtrg5 mqwrite /Vbias 0xff 
-	    bin/setVbias $opts -c 1-102 -r 1-5 -V $V0 $netdev >/dev/null
+		echo "lighting up row $row"
+		bin/setVbias $opts -c 1-6 -r $row -g $gain_pC $netdev >> $tracefile
+		ssh halld@halldtrg5 mqwrite /Vbias 0x0$row
+		sleep $delay
+		ssh halld@halldtrg5 mqwrite /Vbias 0xff
+		bin/setVbias $opts -c 1-6 -r 1-5 -V $V0 $netdev >/dev/null
 	done
-    done
+else
+	for col in 1 2 3; do
+		for row in 1 2 3 4 5; do
+			chan=`echo $row $col | awk '{printf("%2.2x", 1+($1-1)+($2-1)*5)}'`
+			cols=`echo $col | awk '{printf("%d,%d", $1, $1+3)}'`
+			echo "selecting channel $chan"
+			bin/setVbias $opts -c $cols -r $row -g $gain_pC $netdev >>$tracefile
+			ssh halld@halldtrg5 mqwrite /Vbias 0x01$chan
+			sleep $delay
+			ssh halld@halldtrg5 mqwrite /Vbias 0xff 
+			bin/setVbias $opts -c 1-6 -r 1-5 -V $V0 $netdev >/dev/null
+		done
+	done
 fi
 
 echo "Trace of query packets during above sequences was:"
