7b1d550542331eb178eb15a56d88fb9f14146a40
Richard Jones <rjones30@gmail.com>
2018-08-23 18:06:12 -0400

    - major enhancements to the TAGMbias peak fitter [rtj]

--- fitpeaks.py
+++ fitpeaks.py
@@ -11,128 +11,166 @@
 
 import os
 import re
-import argparse
 from ROOT import *
 from array import array
+import numpy
 import time
 
 minEntries = 1200
-maxEntries = 10000
+maxEntries = 100000
 
-def main():
-   # Use argparse if required
-   # maybe use config file that has run number next to row and voltage
+# empirical factor to convert from fit slope(adc_peak/V) to gain(pF)
+fit_slope_to_gain_pF = 0.011 # pC per adc_peak
 
-   gval = [0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45]
-   gset = [['40145', '40146', '40147', '40148', '40149'],
-           ['40150', '40151', '40152', '40153', '40154'],
-           ['40037', '40038', '40039', '40040', '40041'],
-           ['40155', '40156', '40157', '40158', '40159'],
-           ['40042', '40043', '40044', '40045', '40046'],
-           ['40160', '40161', '40162', '40163', '40164'],
-           ['40047', '40048', '40049', '40050', '40051']]
+# dark rate runs taken in January, 2018
+gval = [0.15, 0.20, 0.25, 0.30, 0.35, 0.40, 0.45]
+gset = [['40145', '40146', '40147', '40148', '40149'],
+        ['40150', '40151', '40152', '40153', '40154'],
+        ['40037', '40038', '40039', '40040', '40041'],
+        ['40155', '40156', '40157', '40158', '40159'],
+        ['40042', '40043', '40044', '40045', '40046'],
+        ['40160', '40161', '40162', '40163', '40164'],
+        ['40047', '40048', '40049', '40050', '40051']]
 
-   files = sorted([f for f in os.listdir('.') \
-                   if f.split('.')[-1] == 'root' and \
-                      re.match(r"^TAGMbias_", f) ])
-   conf = 'setVbias_fulldetector-2-23-2017.conf'
+# dark rate runs taking in August, 2018
+gval = [0.20, 0.25, 0.30, 0.35, 0.40, 0.45]
+gset = [['50195', '50196', '50197', '50198', '50199'],
+        ['50180', '50181', '50182', '50183', '50184'],
+        ['50200', '50201', '50202', '50203', '50204'],
+        ['50185', '50186', '50187', '50188', '50189'],
+        ['50205', '50206', '50207', '50208', '50209'],
+        ['50190', '50191', '50192', '50193', '50194']]
 
-   outfile = open('fitpeaks.txt', 'w')
+conf = 'setVbias_fulldetector-1-30-2018.conf'
 
+peak_fit_query = False
+
+latest_results = {}
+
+def Fit1(row, col):
+   """
+   Fit a full set of single-pixel dark pulse spectra and write out
+   a new set of calibration constants into fitpeaks.txt, overwriting
+   the file if it exists already.
+   """
    graph = TGraphErrors(len(gset))
+   p = [0] * len(gset)
+   V = [0] * len(gset)
+   for ig in range(len(gset)):
+      fin = TFile("TAGMbias_{0}.root".format(gset[ig][row-1]))
+      hin = fin.Get("h_spectra_" + str(col))
+      print hin
 
-   gskip = 0
-   for row in range(1,6):
-      fin = [0] * len(gset)
-      for ig in range(len(gset)):
-           fin[ig] = TFile([f for f in files if gset[ig][row-1] in f][0])
+      # Check for bad electronic channels
+      # if GetNumberPeaks(h25) < 2: continue
+      # if GetNumberPeaks(h35) < 2: continue
+      # if GetNumberPeaks(h45) < 2: continue
+      # Add some kind of error recording
 
-      for col in range(1,103):
-           p = [0] * len(gset)
-           V = [0] * len(gset)
-           for ig in range(len(gset)):
-              hin = fin[ig].Get("h_spectra_" + str(col))
+      # Get peaks from each histogram
+      p[ig] = GetPeak(hin)
+      if p[ig] < 0:
+         print 'Fit failed for row', row, ' col ', col,
+         print 'gain setting', gval[ig], 'entries', hin.GetEntries()
+         p[ig] = 0
 
-              # Check for bad electronic channels
-              # if GetNumberPeaks(h25) < 2: continue
-              # if GetNumberPeaks(h35) < 2: continue
-              # if GetNumberPeaks(h45) < 2: continue
-              # Add some kind of error recording
+      # Get voltages
+      V[ig] = GetVoltage(gval[ig], row, col, conf)
 
-              # Get peaks from each histogram
-              p[ig] = GetPeak(hin)
-              if p[ig] < 0:
-                 print 'Fit failed for row', row, ' col ', col,
-                 print 'gain setting', gval[ig], 'entries', hin.GetEntries()
-                 p[ig] = 0
+      # Load values into graph
+      graph.SetPoint(ig, V[ig], p[ig])
+      if p[ig] == 0:
+         graph.SetPointError(ig, 0, 0)
+         continue
+      ncounts = hin.GetEntries()
+      if ncounts > minEntries and ncounts < maxEntries:
+         graph.SetPointError(ig, 0.01, minEntries/hin.GetEntries())
+      else:
+         graph.SetPointError(ig, 0, 0)
 
-              # Get voltages
-              V[ig] = GetVoltage(gval[ig], row, col, conf)
+      # If top points are divergent, kill them off
+      for ig in range(3, len(gset)):
+         ey_ = graph.GetErrorY(ig-3)
+         ey0 = graph.GetErrorY(ig-2)
+         ey1 = graph.GetErrorY(ig-1)
+         ey2 = graph.GetErrorY(ig)
+         if ey_ * ey0 * ey1 * ey2 == 0:
+            continue
+         x0 = Double()
+         y0 = Double()
+         graph.GetPoint(ig-2, x0, y0)
+         x1 = Double()
+         y1 = Double()
+         graph.GetPoint(ig-1, x1, y1)
+         x = Double()
+         y = Double()
+         graph.GetPoint(ig, x, y)
+         slope = (y1 - y0) / (x1 - x0)
+         if slope < 5:
+            continue
+         yline = y0 + (x - x0) * slope
+         if y > yline * 1.2:
+            while ig < len(gset):
+               graph.SetPointError(ig, 0, 0)
+               ig += 1
+            break
 
-              # Load values into graph
-              graph.SetPoint(ig, V[ig], p[ig])
-              ncounts = hin.GetEntries()
-              if ncounts > minEntries and ncounts < maxEntries:
-                 graph.SetPointError(ig, 0.01, minEntries/hin.GetEntries())
-              else:
-                 graph.SetPointError(ig, 0, 0)
+   # Fit TGraph
+   fun1 = TF1("fun1", Hyperfit, 50, 100, 3)
+   fun1.SetParameter(0, 71.0)
+   fun1.SetParameter(1, 25)
+   fun1.SetParameter(2, 5)
+   ptr = graph.Fit(fun1, "s")
+   xint = ptr.Parameters()[0]
+   yint = ptr.Parameters()[1]
+   rasymp = ptr.Parameters()[2]
+   gref = 40
+   Vref = xint + (gref**2 - yint**2)**0.5 / rasymp
+   slope = (Vref - xint) * rasymp**2 / gref
+   Vbd = Vref - gref / slope
+   print Vbd, slope
+   graph.SetTitle("gain curve for row " + 
+                  str(row) + " col " + str(col))
+   graph.Draw("A*")
+   xasym = numpy.array([Vref - 2, Vref + 2], dtype=float)
+   yasym = numpy.array([gref - 2*slope, gref + 2*slope], dtype=float)
+   gasym = TGraph(2, xasym, yasym)
+   gasym.SetLineColor(kRed)
+   gasym.SetLineStyle(9)
+   gasym.Draw("l")
+   c1.Update()
 
-           # If top points are divergent, kill them off
-           for ig in range(3, len(gset)):
-              ey_ = graph.GetErrorY(ig-3)
-              ey0 = graph.GetErrorY(ig-2)
-              ey1 = graph.GetErrorY(ig-1)
-              ey2 = graph.GetErrorY(ig)
-              if ey_ * ey0 * ey1 * ey2 == 0:
-                 continue
-              x0 = Double()
-              y0 = Double()
-              graph.GetPoint(ig-2, x0, y0)
-              x1 = Double()
-              y1 = Double()
-              graph.GetPoint(ig-1, x1, y1)
-              x = Double()
-              y = Double()
-              graph.GetPoint(ig, x, y)
-              slope = (y1 - y0) / (x1 - x0)
-              if slope < 5:
-                 continue
-              yline = y0 + (x - x0) * slope
-              if y > yline * 1.2:
-                 while ig < len(gset):
-                    graph.SetPointError(ig, 0, 0)
-                    ig += 1
-                 break
+   global peak_fit_query
+   ans = raw_input("r to redo, enter to accept? ")
+   if len(ans) > 0 and ans[0] == 'r':
+      peak_fit_query = 1
+      return Fit1(row, col)
+   elif len(ans) > 0 and ans[0] == 'p':
+      c1.Print("fitpeaks_{0}_{1}.png".format(row,col))
+   else:
+      peak_fit_query = 0
 
-           # Fit TGraph
-           if p[-1] > 0:
-              ptr = graph.Fit("pol1", "sq")
-              slope = ptr.Parameters()[1]
-              yint = ptr.Parameters()[0]
-              xint = -yint/slope
-           else:
-              slope = 0
-              xint = 0
-           if gskip > 0:
-              gskip -= 1
-           else:
-              graph.SetTitle("gain curve for row " + 
-                             str(row) + " col " + str(col))
-              graph.Draw("A*")
-              c1.Update()
-              ans = raw_input("number to skip before next check [0]? ")
-              try:
-                 gskip = int(ans)
-              except:
-                 gskip = 0
+   gain_pF = slope * fit_slope_to_gain_pF
+   latest_results[(row,col)] = "{0} {1}".format(Vbd, gain_pF)
 
-           chan = GetChannel(row, col)
-           geoaddr = GetGeoAddr(col)
-           outfile.write(' {0} {1} {2} {3} {4} {5}\n'.format(
-                         row-1, col-1, chan, geoaddr, xint, slope))
-   outfile.close()
+def Hyperfit(var, par):
+   """
+   Hyperbolic fit function to apply to graphs of single-pixel
+   pulse height maximum versus bias voltage. This should normally
+   be a straight line, but there is shift toward high pulse height
+   at low values of Vbias that comes from the fadc readout threshold
+   suppressing pulses below a certain minimum pulse height.
+   """
+   V = var[0]
+   V0 = par[0]
+   y0 = par[1]
+   slope = par[2]
+   return ((slope * (V - V0))**2 + y0**2)**0.5
 
 def GetPeak(h):
+   """
+   Find the x value of the maximum of the primary peak in histogram h
+   """
    h.RebinX(4)
    maximum = h.GetBinCenter( h.GetMaximumBin() )
    if h.GetEntries() < 1:
@@ -147,16 +185,19 @@ def GetPeak(h):
       sigma = -1
    #if (sigma > 6):
    #    mean = -1
-
-   #h.Draw()
-   #c1.Update()
-   #raw_input("press enter to continue: ")
+   if peak_fit_query:
+      h.Draw()
+      c1.Update()
+      ans = raw_input("x to reject, enter to accept? ")
+      if len(ans) > 0 and ans[0] == 'x':
+         return 0
    return float(mean)
 
 def GetNumberPeaks(h):
-   # Use this to make sure there are multiple peaks
-   # If not, mark as problem channel
-
+   """
+   Check that there are multiple peaks in histogram h, 
+   and if not, mark as problem channel.
+   """
    if (h.GetEntries() < 100):
       return 0
 
@@ -166,20 +207,32 @@ def GetNumberPeaks(h):
    return npeaks
 
 def GetGeoAddr(col):
+   """
+   Look up the geographical address of the board that contains
+   the sum circuit for TAGM column col.
+   """
    baseAddr = int('8e', 16)
    geoaddr = baseAddr + int(col - 1)/6
 
    return hex(geoaddr).split('x')[-1]
 
 def GetChannel(row, col):
-   # Take cable swap into account (col 3 is actually col 1 electronically)
-
+   """
+   Look up the fadc250 board channel number that digitizes
+   the sum signal for TAGM column col, taking the cable swap
+   into account, eg. col 3 is actually col 1 electronically.
+   """
    newcol = 3*(1 + (col - 1)/3) - (col - 1)%3
 
    channel = 5*( (newcol - 1) % 6 ) + (row - 1)
    return channel
 
 def GetVoltage(g, row, col, conf):
+   """
+   Compute the Vbias voltage that would be applied to the SiPM
+   that reads out fiber row,col if it is set to gain g, using the
+   calibration contained in setVbias.conf file conf.
+   """
    conf_file = open(conf, 'r')
    for line in conf_file:
       if not line.split()[0][0].isdigit():
@@ -193,5 +246,26 @@ def GetVoltage(g, row, col, conf):
    conf_file.close()
    return float(voltage)
 
-if __name__ == "__main__":
-   main()
+def Write():
+   """
+   Write new results to a file in the standard format of setVbias.conf
+   using the prior conf file as a source of the light yield constants,
+   since these cannot be evaluated based on dark count data.
+   """
+   outfile = open('fitpeaks.txt', 'w')
+   conf_file = open(conf, 'r')
+   for line in conf_file:
+      if not line.split()[0][0].isdigit():
+         outfile.write(line)
+         continue
+      (geo, cha, col, row, Vbd, gpF, ypi) = line.split()
+      r = int(row)
+      c = int(col)
+      if (r,c) in latest_results:
+         line = line[0:42]
+         line += "{0:13.3f}".format(float(latest_results[(r,c)].split()[0]))
+         line += "{0:12.3f}".format(float(latest_results[(r,c)].split()[1]))
+         line += "{0:16.2f}".format(float(ypi))
+         line += "\n"
+      outfile.write(line)
+   outfile.close()
