fb9540518c006c0e91d520c2d2c899f053675b24
Richard Jones <rjones30@gmail.com>
2023-01-21 12:32:50 -0500

    * latest updates to the condor submit scripts for the TAGM_trees plugin [rtj]

--- TAGMtrees.bash
+++ TAGMtrees.bash
@@ -1,36 +1,48 @@
 #!/bin/bash
 
+nskip=0
+nevents=999999999
+nthreads=4
+batch=0
+
 echo Running on `hostname`
 
 source setup.sh
 echo $LD_LIBRARY_PATH
 ldd `which hd_root`
 
-pnfspath=/pnfs4/phys.uconn.edu/data
-rawdata=/Gluex/calibration/TAGM/rawdata
+xrootdpath=root://nod25.phys.uconn.edu/gluex/uconn0
+#rawdata=/rawdata/TAGM_calib
+rawdata=/rawdata/winter_2023
 
 if [[ $# > 0 ]]; then
     line=`expr $1 + 1`
     run=`head -n $line TAGMtrees_runs.list | tail -n 1`
-    export infile=`ls $pnfspath/$rawdata/Run0$run/*.evio`
+    indir=$(echo $run | awk '{printf("'$xrootdpath$rawdata'/Run%06d", $1)}')
+    infiles=""
+    for infile in $(ls $indir | grep "\.evio$"); do
+        infiles="$infiles $indir/$infile"
+    done
 else
     echo "usage: run_hdroot.sh <sequence number>"
     exit 1
 fi
-if [[ ! -r $infile ]]; then
-    echo "Error - unable to open input file $infile"
-    exit 1
-fi
 
-echo $HALLD_HOME/$BMS_OSNAME/bin/hd_root -PJANA:BATCH_MODE=1 -PPRINT_PLUGIN_PATHS=1 -PPLUGINS=TAGM_trees -PTHREAD_TIMEOUT_FIRST_EVENT=3000 -PTHREAD_TIMEOUT=3000 -PEVIO:RUN_NUMBER=$run $infile
+echo $HALLD_HOME/$BMS_OSNAME/bin/hd_root -PJANA:BATCH_MODE=1 -PPRINT_PLUGIN_PATHS=1 -PPLUGINS=TAGM_trees -PTHREAD_TIMEOUT_FIRST_EVENT=3000 -PTHREAD_TIMEOUT=3000 -PEVIO:RUN_NUMBER=$run $infiles
 $HALLD_HOME/$BMS_OSNAME/bin/hd_root \
   -PPRINT_PLUGIN_PATHS=1 \
-  -PJANA:BATCH_MODE=1 \
+  -PJANA:BATCH_MODE=$batch \
   -PPLUGINS=TAGM_trees \
+  -PEVIO:SYSTEMS_TO_PARSE=TAGM \
+  -PEVIO:SYSTEMS_TO_PARSE_FORCE=1 \
   -PEVIO:RUN_NUMBER=$run \
+  -PEVENTS_TO_SKIP=$nskip \
+  -PEVENTS_TO_KEEP=$nevents \
   -PTHREAD_TIMEOUT_FIRST_EVENT=300 \
   -PTHREAD_TIMEOUT=300 \
-  $infile
+  -PNTHREADS=4 \
+  --nthreads=4 \
+  $infiles
 retcode=$?
 
 if [[ $retcode = 0 ]]; then
